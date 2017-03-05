f996a0d41b23027eb1870ddfb35bfbc98bc4b417
Bing <Bing.rao@outlook.com>
2017-03-04 22:17:02 -0500

    initial a children project

---
+++ ucf-archetype-children/src/main/resources/archetype-resources/src/test/scala/ScalaTestApp.scala
@@ -0,0 +1,13 @@
+package ${package};
+/**
+  * @author
+  */
+import org.junit.Test
+import org.junit.Assert._
+
+class ScalaTestAPP {
+  @Test def testAdd() {
+    println("Hello World From Scala");
+    assertTrue(true);
+  }
+}
