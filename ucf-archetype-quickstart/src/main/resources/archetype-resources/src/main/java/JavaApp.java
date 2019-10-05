9d8089ba63a554fa59a5280b2294a1ee392d225e
Bing Rao <Bing.Rao@outlook.com>
2019-10-05 07:20:52 -0400

    1. add printhello func in java code, 2 update readme docs

--- ucf-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/JavaApp.java
+++ ucf-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/JavaApp.java
@@ -4,11 +4,11 @@ package ${package};
  * @author 
  */
 public class JavaApp {
+    public static void printHello(){
+        System.out.println("Hello World from Java");
+    }
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-    public void printJavaHello(){
- 	   System.out.println("Hello World from Java");
+        printHello();
     }
 }
