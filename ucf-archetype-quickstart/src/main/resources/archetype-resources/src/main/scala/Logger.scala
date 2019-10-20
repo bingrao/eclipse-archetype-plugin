b445fc1908444a16eb33316c0e0479ab4d9af15e
Bing Rao <bing.rao@outlook.com>
2019-10-19 21:25:10 -0500

    add logger functions based on log4j

---
+++ ucf-archetype-quickstart/src/main/resources/archetype-resources/src/main/scala/Logger.scala
@@ -0,0 +1,13 @@
+package ${package}
+import  org.apache.log4j.Logger
+
+class Logger(name:String){
+  private val logger = Logger.getLogger(name)
+
+  def info(message:String) = logger.info(message)
+  def warn(message:String) = logger.warn(message)
+  def debug(message:String) = logger.debug(message)
+  def error(message:String) = logger.error(message)
+
+  def isDebugEnabled = logger.isDebugEnabled
+}
