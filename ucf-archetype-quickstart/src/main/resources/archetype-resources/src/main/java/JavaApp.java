6649aac8ba8b1d6b16c2d6e54cab92554f86edaa
Bing Rao <bing.rao@outlook.com>
2019-10-24 03:13:48 -0400

    debug java in log4j

--- ucf-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/JavaApp.java
+++ ucf-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/JavaApp.java
@@ -7,7 +7,7 @@ import ${package}.utils.Log;
 public class JavaApp {
     public static Log log = new Log("JavaApp");
     public static void printHello(){
-        log.info("Hello World from Java");
+        log.info("Hello World from Java",true);
     }
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
