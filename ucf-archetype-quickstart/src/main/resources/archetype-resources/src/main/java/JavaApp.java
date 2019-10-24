60e441290a0b27a774082600f51a131e3303cbad
Bing Rao <bing.rao@outlook.com>
2019-10-24 01:31:47 -0500

    add log to javaAPP

--- ucf-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/JavaApp.java
+++ ucf-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/JavaApp.java
@@ -1,11 +1,13 @@
 package ${package};
+import ${package}.utils.Log;
 
 /**
  * @author 
  */
 public class JavaApp {
+    public static Log log = new Log("JavaApp");
     public static void printHello(){
-        System.out.println("Hello World from Java");
+        log.info("Hello World from Java");
     }
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
